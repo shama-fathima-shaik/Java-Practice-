e4d1ab464c31fb23a9fbe877fd00c30c771e0e67
SHAMA FATHIMA SHAIK <shaikshamafathima@gmail.com>
2026-07-28 20:38:31 +0530

    Create Day 2_print_output_2.java

---
+++ Day 2_print_output_2.java
@@ -0,0 +1,10 @@
+class GFG{
+  public static void main(String[] args){
+    int num=10, num2=20;
+    System.out.print("The Addution of");
+    System.out.print("num1 + "and" + num2 + "is:");
+    System.out.println("num1 + num2");
+  }
+}
+
+
