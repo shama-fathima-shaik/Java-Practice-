aaf078752e7ab55b0462e4c706035b3caebd59a8
SHAMA FATHIMA SHAIK <shaikshamafathima@gmail.com>
2026-07-29 20:48:43 +0530

    Create Day 1_taking_input_1.java

---
+++ Day 1_taking_input_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Geeks{
+  public static void main(String[] args)
+  {
+    Scanner scn = new Scanner(System.in);
+    System.out.print("Enter First Number: ");
+    int a = scn.nextInt();
+    System.out.print("Enter Second Number: ");
+    int b = scn.nextInt();
+    System.out.println("Sum: " + (a + b));
+    scn.close();
+  }
+}
+
+    
