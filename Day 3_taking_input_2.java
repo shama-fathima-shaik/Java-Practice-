0c7bdfc4c255607537ccf91cb969d6546def7a8c
SHAMA FATHIMA SHAIK <shaikshamafathima@gmail.com>
2026-07-29 21:57:06 +0530

    Create Day 3_taking_input_2.java

---
+++ Day 3_taking_input_2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class greeks{ 
+   public static void main(String[] args){
+  Scanner scn = new Scanner(System.in);
+  System.out.print("Enter a Sentence: ");
+  String entence = scn.nextLine();
+  System.out.println("Enterted Sentence: " + sentence);
+  System.out.print("Enter an integer: ");
+  int x = Integer .parseInt(scn.nextLine());
+  System.out.println("Entered Integer: " + x);
+  System.out.print("Enter a float value: ");
+  float f = Float.parseFloat(scn.nextLine());
+  System.out.println("Entered Float Value: " + f);
+  scn.close();
+}
+}
+
+
+    
+  
