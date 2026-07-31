4d76accc5642bb3c2810db93cd893e297b717ee4
SHAMA FATHIMA SHAIK <shaikshamafathima@gmail.com>
2026-07-31 21:14:10 +0530

    Create Day 4_variables_2.java

---
+++ Day 4_variables_2.java
@@ -0,0 +1,12 @@
+class Greeks{
+  public static void main(String[] args){
+    float si = 5.5f;
+    int t = 10;
+    int s = 20;
+    char var = 'h';
+    System.out.println("Simple Interest: " + si);
+    System.out.println("Speed: " + s);
+    System.out.println("Time: " + t);
+    System.out.println("Character: " + var);
+  }
+}
