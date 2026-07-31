14d6f3d1f373ccf74d94c87d48e7ad5cecfcbfec
SHAMA FATHIMA SHAIK <shaikshamafathima@gmail.com>
2026-07-31 20:47:02 +0530

    Create Day 4_keywords.java

---
+++ Day 4_keywords.java
@@ -0,0 +1,12 @@
+class Greks{
+  public static void main(String[] args)
+  {
+    final int x = 10;
+    if(x > 10){
+      System.out.println("Failed)
+        }
+    else{
+      System.out.println("Sucessful demonstration"+ of keywords.");
+    }
+  }
+}
