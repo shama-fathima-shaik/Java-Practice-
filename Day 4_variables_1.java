c6e5b6a1812bb9967b6035deb042b9b71c2ee175
SHAMA FATHIMA SHAIK <shaikshamafathima@gmail.com>
2026-07-31 21:09:21 +0530

    Create Day 4_variables_1.java

---
+++ Day 4_variables_1.java
@@ -0,0 +1,17 @@
+class Greeks{
+  public static void main(String[] args{
+    int age = 25;
+    String name = "GreeksforGreeks";
+    double salary 50000.50;
+    System.out.println("Age: " + age);
+    System.out.println("Name: " + name);
+    System.out.println("Salary: " + salary);
+  }
+}
+                  
+
+                          
+    
+    
+                          
+                          
